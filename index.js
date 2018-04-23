8ea1ea557f4d2330819b758d5b22c32180f0a37d
Valerie Moore <periwinkle17@outlook.com>
2018-04-23 17:57:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const app = "I don't do much."
 function destructivelyAppendKitten(array, element) {
   return (kittens.push('Ralph'))
 }
+function destructivelyPrependKitten(array, element) {
+  return (kittens.unshift('Ralph'))
+}
